b0eae68151e10a82d95cf2f38b98808f7e26e533
hmorton77 <harold.m.morton@gmail.com>
2020-10-08 04:02:02 -0400

    added AJAX call for current weather

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var idNum;
 var APIkey = "6dadd3c90772780ec35050af315cf499";
+var queryURL5day;
+var queryURLtoday;
 var inputCombo;
 // convert input to city ID
 $(".submitBtn").on("click", function (event) {
@@ -23,21 +25,32 @@ $(".submitBtn").on("click", function (event) {
   localStorage.setItem("everything", inputCombo);
 
   console.log("items stored!");
-
+  // Check each index in the large JSON object to see if the city/state or city/nation match
   for (var i = 0; i < cityID.length; i++) {
     if (cityName && stateName && cityID[i].name == cityName && cityID[i].state == stateName) {
-      console.log(cityID[i].id);
+      idNum = cityID[i].id;
+      break;
     } else if (cityName && nationName && cityID[i].name == cityName && cityID[i].country == nationName) {
-      console.log(cityID[i].id);
+      idNum = cityID[i].id;
+      break; //(there are 2 cities named Berlin in Germany and it is a pain in the butt to try to differentiate them)
     }
   }
-});
-console.log(cityID[0].name);
-var queryURL5day = "api.openweathermap.org/data/2.5/forecast?q=" + idNum + "&appid=" + APIkey;
+  queryURL5day = "https://api.openweathermap.org/data/2.5/forecast?id=" + idNum + "&units=imperial&appid=" + APIkey;
+  queryURLtoday = "https://api.openweathermap.org/data/2.5/weather?id=" + idNum + "&units=imperial&appid=" + APIkey;
 
-// $.ajax({
-//   url: queryURL5day,
-//   method: "GET",
-// }).then(function (response5day) {
-//   console.log(response5day);
-// });
+  console.log(queryURL5day);
+  // ajax call for 5 day forecast
+  $.ajax({
+    url: queryURL5day,
+    method: "GET",
+  }).then(function (response5day) {
+    console.log(response5day);
+  });
+  // ajax call for current weather
+  $.ajax({
+    url: queryURLtoday,
+    method: "GET",
+  }).then(function (responsetoday) {
+    console.log(responsetoday);
+  });
+});
